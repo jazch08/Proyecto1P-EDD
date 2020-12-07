273a1aa99d99d23e163f6bac9fd01dbbcbea3755
Jorge Zambrano <jaaronzambranoch@outlook.es>
2020-12-07 07:02:50 -0500

    Next y play

--- src/controlador/PantallaPrincipalController.java
+++ src/controlador/PantallaPrincipalController.java
@@ -2,18 +2,25 @@ package controlador;
 
 import Main.Lectura;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
 import java.util.ResourceBundle;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Pos;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import modelo.Cluster;
 import modelo.Pixel;
 import modelo.Punto;
 import operaciones.OperacionesCluster;
@@ -31,37 +38,59 @@ public class PantallaPrincipalController implements Initializable {
     @FXML
     private ImageView play;
     @FXML
-    private ImageView siguiente;
-    
-    boolean cambioPlay = false;
+    private ImageView siguiente;   
     @FXML
     private Slider velocidad;
+    @FXML
+    private VBox PilaDinamica;
+    @FXML
+    private Button cargar;
     
+    public static boolean continuar = true,cambioPlay = true,cargado = false;
+    static PriorityQueue<Cluster> c = OperacionesCluster.cluters;
+    public static Iterator iter;
+    public static VBox pila;
     Thread th;
-
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+         pila=PilaDinamica;
+        cargar.setOnMouseClicked(e->cargar());
+        
+        play.setOnMouseClicked(e -> cargadoPlay());
+        
+        siguiente.setOnMouseClicked(e -> cargadoNext());
+    }
+    
+    private void cargar(){
+        continuar = false;
+        cargado=true;
+        play.setImage(new Image("Archivos/play.png"));
+        cambioPlay = true;
+        OperacionesCluster.cluters.clear();
+        colores.getChildren().clear();
+        PilaDinamica.getChildren().clear();
         HashMap<Punto, Pixel> m = Lectura.leerArc("Matriz_Inicio");
         crearMatrizColor(m);
         OperacionesCluster.dividirPorCluster(m);
-        th = new Thread(new operacionMatriz(velocidad));
-        play.setOnMouseClicked(e -> clickPlay());
+        iter=obtenerIterator();
     }
-
     
-    private void crearMatrizColores(ArrayList<ArrayList<Pixel>> cFilas) {
-        colores.setAlignment(Pos.CENTER);
-        int f=1;
-        for(ArrayList<Pixel> filas : cFilas){
-            HBox elementos = new HBox();
-            elementos.setAlignment(Pos.CENTER);
-            int c=0;
-            for(Pixel e : filas){
-                elementos.getChildren().add(e.getLb());
-                c++;
-            }
-            colores.getChildren().add(elementos);
-            f++;
+    private void cargadoPlay(){
+        if(!cargado){
+            mostrarAlerta("Error de Carga","Error: Aun no ha cargado la matriz",AlertType.ERROR);
+        }
+        else{
+            clickPlay();
+        }
+    }
+    
+    private void cargadoNext(){
+        if(!cargado){
+            mostrarAlerta("Error de Carga","Error: Aun no ha cargado la matriz",AlertType.ERROR);
+        }
+        else{
+            clicNext();
         }
     }
     
@@ -79,18 +108,56 @@ public class PantallaPrincipalController implements Initializable {
     }
     
     private void clickPlay(){
+        
         Image imagen = null;
         if(cambioPlay){
-            imagen = new Image("Archivos/play.png");
+            imagen = new Image("Archivos/pause.png");
             cambioPlay = false;
+            continuar=true;
+            th = new Thread(new operacionMatriz(velocidad));
             th.start();       
         }
         else{
-            imagen = new Image("Archivos/pause.png");
+            imagen = new Image("Archivos/play.png");
             cambioPlay = true;
+            continuar= false;
         }
         play.setImage(imagen);
     }
    
+    private void clicNext(){
+       play.setImage(new Image("Archivos/play.png"));
+       cambioPlay = true;
+       continuar = false ;
+       Next();
+    }
     
+    public static void Next(){
+        if(iter!=null && iter.hasNext()){
+            Pixel pix = (Pixel)iter.next();
+            Label lb = pix.getLb();
+            Platform.runLater(()->lb.setStyle("-fx-border-color: #000000; -fx-background-color: #ffa500;"));
+            Platform.runLater(()->pila.getChildren().add(new Label(pix.toString())));       
+        }
+        else{
+            iter=obtenerIterator();
+        }
+    }
+    
+    private static Iterator obtenerIterator(){
+        Iterator iter0=null;
+        if(!c.isEmpty()){
+            LinkedList<Pixel> listaP = c.poll().getPixeles();
+            iter0 = listaP.iterator();
+        }
+        return iter0;
+    }
+    
+    public static void mostrarAlerta(String titulo, String mensaje, Alert.AlertType e){
+        Alert alert = new Alert(e);
+        alert.setTitle(titulo);
+        alert.setHeaderText(null);
+        alert.setContentText(mensaje);
+        alert.showAndWait();
+    }
 }
--- src/operaciones/operacionMatriz.java
+++ src/operaciones/operacionMatriz.java
@@ -5,23 +5,18 @@
  */
 package operaciones;
 
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.PriorityQueue;
+import controlador.PantallaPrincipalController;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javafx.application.Platform;
-import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
-import modelo.Cluster;
-import modelo.Pixel;
+
 
 /**
  *
  * @author Jorge
  */
 public class operacionMatriz implements Runnable{
-    Slider velocidad;
+    private Slider velocidad;
 
     public operacionMatriz(Slider velocidad) {
         this.velocidad = velocidad;
@@ -29,26 +24,15 @@ public class operacionMatriz implements Runnable{
     
     @Override
     public void run() {
-       PriorityQueue<Cluster> c = OperacionesCluster.cluters;
-       while(!c.isEmpty()){
-           
-           LinkedList<Pixel> listaP = c.poll().getPixeles();
-           Iterator iter = listaP.iterator();
-           while(iter.hasNext()){
-               Pixel pix = (Pixel)iter.next();
-               try {
-                   long factor = (long)velocidad.getValue();
-                   
-                   Label lb = pix.getLb();
-                   Platform.runLater(()->lb.setStyle("-fx-border-color: #000000; -fx-background-color: #ffa500;"));
-                   
-                   Thread.sleep(2000/factor);
-               } catch (InterruptedException|NullPointerException ex) {
-                   Logger.getLogger(operacionMatriz.class.getName()).log(Level.SEVERE, null, ex);
-               }
+        while(PantallaPrincipalController.continuar){
+            try {
+                long factor = (long)velocidad.getValue();
+                PantallaPrincipalController.Next();
+                Thread.sleep(2000/factor);
+            } catch (InterruptedException|NullPointerException ex) {
+                Logger.getLogger(operacionMatriz.class.getName()).log(Level.SEVERE, null, ex);
+            }
                
-           }
-       }
+        }
     }
-    
 }
